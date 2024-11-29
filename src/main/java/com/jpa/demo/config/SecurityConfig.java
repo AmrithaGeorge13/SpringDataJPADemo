ae6a6d61ca47d98d1887adc6354b9c796ee3f03d
amrithag <amrithgeorge@gmail.com>
2024-11-29 06:30:36 +0530

    spring SecurityConfig cosmetic change

--- src/main/java/com/jpa/demo/config/SecurityConfig.java
+++ src/main/java/com/jpa/demo/config/SecurityConfig.java
@@ -2,15 +2,15 @@ package com.jpa.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()) // Disable CSRF for POST requests
+        http.csrf(AbstractHttpConfigurer::disable) // Disable CSRF for POST requests
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/employees/**").permitAll() // Allow /employees endpoint
                         .anyRequest().authenticated()); // Protect other endpoints
